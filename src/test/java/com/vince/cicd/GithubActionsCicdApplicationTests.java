7ad9df27674109db612999c2f1b901a8b51220f3
vincekashaka <vincekashaka@gmail.com>
2022-08-14 21:52:33 +0200

    code

---
+++ src/test/java/com/vince/cicd/GithubActionsCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vince.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
